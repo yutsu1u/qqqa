62ea092d9bf4e2afb0ea228a59f9916c013bcf49
Ilya <ateilaz@mail.ru>
2024-12-05 23:28:26 +0300

    Исправлены ошибки линтинга и тестов

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  const greeting = "Иванов Илья Александрович";
+  const greeting = "Иванов Илья Александровичаа";
 
   console.log('This is App ');
   return (
