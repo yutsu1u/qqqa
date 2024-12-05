b64a79572f12ba6f85e21e027c566d0913e95c7b
Ilya <ateilaz@mail.ru>
2024-12-05 23:47:17 +0300

    Добавлено ФИО

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  const greeting = "Иванов Илья Александрович";
+  const greeting = "Иванова Ильи Александровичаа";
 
   console.log('This is App ');
   return (
